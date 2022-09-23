544acb7ca52424f91463461be5a1d4fa4905288a
Luis Fernando Machado Poletti Valle <lmachado@student.ethz.ch>
2022-09-23 20:34:02 +0200

    Split target selection vs. 2PCF computation; add bitmask logic for BGS

---
+++ desiimaginganalysis/compute_2pcf.py
@@ -0,0 +1,28 @@
+from select_imaging_targets import *
+
+import numpy as np
+
+import Corrfunc
+from Corrfunc.mocks.DDtheta_mocks import DDtheta_mocks
+from Corrfunc.utils import convert_3d_counts_to_cf
+
+# Get random RAs and DECs
+# TODO Need to use the DR9 randoms instead of np.random
+rand_N = 2000
+random_RAs = 360 * np.random.rand(rand_N)
+random_DECs = 180 * np.random.rand(rand_N) - 90
+
+# Compute correlation function
+nbins = 10
+bins = np.linspace(0.1, 10.0, nbins + 1)
+nthreads = 1
+
+DD_counts = DDtheta_mocks(1, nthreads, bins, RAs, DECs)
+RR_counts = DDtheta_mocks(1, nthreads, bins, random_RAs, random_DECs)
+DR_counts = DDtheta_mocks(0, nthreads, bins, RAs, DECs, RA2=random_RAs, DEC2=random_DECs)
+
+wtheta = convert_3d_counts_to_cf(N, N, rand_N, rand_N,
+DD_counts, DR_counts,
+DR_counts, RR_counts)
+
+print(wtheta)
--- desiimaginganalysis/download_sweep_data.py
+++ desiimaginganalysis/download_sweep_data.py
@@ -3,7 +3,7 @@ import subprocess
 
 REGION = "south" # north or south
 
-FILENAME = "sweep-040p025-050p030.fits"
+FILENAME = "sweep-020m050-030m045.fits"
 PATH = f"https://portal.nersc.gov/cfs/cosmo/data/legacysurvey/dr9/{REGION}/sweep/9.0/{FILENAME}"
 
-subprocess.run(["wget", "--limit-rate=1m", PATH])
+subprocess.run(["wget", "--limit-rate=100m", PATH])
