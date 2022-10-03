48024c20b473978e1a24e41d0ddc1e9547181aeb
Luis Fernando Machado Poletti Valle <lmachado@student.ethz.ch>
2022-10-03 13:34:08 +0200

    Create mask from DR9 survey brick files

--- desiimaginganalysis/select_imaging_targets.py
+++ desiimaginganalysis/select_imaging_targets.py
@@ -1,8 +1,12 @@
 from desitarget.cuts import select_targets
 from desitarget.targets import desi_mask, bgs_mask
 
-FILES = ["sweep-040p025-050p030.fits", "sweep-260p025-270p030.fits", "sweep-010p000-020p005.fits", "sweep-020p025-030p030.fits", "sweep-240p025-250p030.fits"]
-#FILES = ["sweep-020m050-030m045.fits"]
+PATH_TO_SWEEP_FILES = "/cluster/home/lmachado/msc-thesis/dr9/sweepfiles/"
+
+FILENAMES = ["sweep-040p025-050p030.fits", "sweep-260p025-270p030.fits", "sweep-010p000-020p005.fits", "sweep-020p025-030p030.fits", "sweep-240p025-250p030.fits"]
+#FILENAMES = ["sweep-020m050-030m045.fits"]
+
+FILES = [f"{PATH_TO_SWEEP_FILES}/{filename}" for filename in FILENAMES]
 
 # Select BGS targets from sweep files
 targets, infiles = select_targets(
