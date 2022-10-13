41c6f1cf4f2dfe441f39e92190d51cce721351f5
Luis Fernando Machado Poletti Valle <lmachado@student.ethz.ch>
2022-10-13 22:59:21 +0200

    Compute healpy pixels from RAs and DECs. Overplot targets and footprint in mollview map.

--- desiimaginganalysis/plot_sky_map.py
+++ desiimaginganalysis/plot_sky_map.py
@@ -2,12 +2,20 @@ from desiutil.plots import prepare_data, init_sky, plot_grid_map, plot_healpix_m
 import healpy as hp
 from matplotlib import pyplot as plt
 from matplotlib.cm import get_cmap
+from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
+
+# If False, only plot the footprint
+DISPLAY_TARGETS = True
+
+REGION = "north"
 
 plt.rcParams['font.size'] = '12'
 
 nside = 512
 npix = hp.nside2npix(nside)
+
+pixel_area = hp.pixelfunc.nside2pixarea(nside, degrees=True)
 
 # Load footprints
 with open(f"/cluster/scratch/lmachado/DataProducts/footprint/pixels_in_north_footprint_for_nside_{nside}.npy", "rb") as f:
@@ -30,20 +38,41 @@ print("intersection: ", area_footprint_intersection)
 
 m = np.zeros(npix)
 
-m[footprint_north] = 0.5
-m[footprint_south] = 0.5
+m[footprint_north] = np.nan
+m[footprint_south] = np.nan
 
-cmap = get_cmap("gray_r").copy()
+# Make custom cmap,
+# with white as the first color
+# so the background values are zero
+cmap_jet = get_cmap("jet").copy()
+cmap = LinearSegmentedColormap.from_list(
+    "",
+    ["white"] + list(
+        cmap_jet(np.linspace(0, 1, 7))
+    ),
+)
+
+# If requested, overplot histogram with target counts in each pixel
+if DISPLAY_TARGETS:
+    with open(f"/cluster/scratch/lmachado/DataProducts/targets/{REGION}/targets_HPXPIXEL_HPXNSIDE_{nside}.npy", "rb") as f:
+        target_pixels = np.load(f)
+
+    idx, counts = np.unique(target_pixels, return_counts=True)
+
+    # fill the fullsky map
+    m[idx] = counts / pixel_area # Convert from counts to target density (deg^-2)
 
 hp.mollview(
     m,
+    fig=0,
     nest=True,
-    title=f"Legacy Survey DR9 Footprint, nside = {nside}",
-    rot=[120,0],
-    cbar=False,
+    title=f"Legacy Survey (LS) DR9 Footprint, nside = {nside}",
+    unit=r"BGS Target Density [$deg^{-2}$]",
+    rot=[120, 0],
+    badcolor="gray",
+    cbar=True,
     cmap=cmap,
     min=0,
-    max=1,
 )
 
 # Display lines for Dec=+32 and Dec=-18
@@ -53,13 +82,16 @@ ras = np.linspace(0, 360, 700)
 north_south_split_radians = 32
 desi_southmost_radians = -15
 
-hp.projscatter(ras, [north_south_split_radians]*len(ras), lonlat=True, c="black", s=2, label="North-South Split in Legacy Survey")
-hp.projscatter(ras, [desi_southmost_radians]*len(ras), lonlat=True, c="red", s=2, label="DESI Southmost Coverage")
+hp.projscatter(ras, [north_south_split_radians]*len(ras), lonlat=True, c="black", s=2, label="N/S Split in LS")
+hp.projscatter(ras, [desi_southmost_radians]*len(ras), lonlat=True, c="red", s=2, label="DESI Southmost")
 
 
 hp.graticule()
 
-plt.legend(loc="lower right", bbox_to_anchor=(1, -0.16))
+plt.legend(
+    loc="lower right",
+    bbox_to_anchor=(1.02, -0.08)
+)
 
-plt.savefig("/cluster/home/lmachdo/msc-thesis/desiimaginganalysis/images/footprint.pdf")
+plt.savefig("/cluster/home/lmachado/msc-thesis/desiimaginganalysis/images/footprint.pdf")
 plt.show()
