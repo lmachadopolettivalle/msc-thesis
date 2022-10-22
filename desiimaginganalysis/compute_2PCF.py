0235e3fef93365e6071f06e02231cc60978519c2
Luis Fernando Machado Poletti Valle <lmachado@student.ethz.ch>
2022-10-23 01:31:32 +0200

    Compute and plot 2PCF with bins in r-magnitude

--- desiimaginganalysis/compute_2PCF.py
+++ desiimaginganalysis/compute_2PCF.py
@@ -4,7 +4,7 @@ import Corrfunc
 from Corrfunc.mocks.DDtheta_mocks import DDtheta_mocks
 from Corrfunc.utils import convert_3d_counts_to_cf
 
-from load_processed_target_data import load_processed_target_data
+from load_processed_target_data import load_processed_target_data, BGS_BRIGHT, BGS_FAINT
 from mask import mask
 
 REGION = "north"
@@ -56,11 +56,24 @@ randoms = {
     for k, v in randoms.items()
 }
 
-# Count number of targets and randoms
-targets_count = len(targets["RA"])
-randoms_count = len(randoms["RA"])
 
-print(targets_count, randoms_count)
+# Compute 2PCF for each group of targets
+bgs_category_target_ids = {
+    "bright": np.where(targets["BGS_TARGET"] == BGS_BRIGHT)[0],
+    "faint": np.where(targets["BGS_TARGET"] == BGS_FAINT)[0],
+}
+
+rmag_bins = {
+    "bright": [
+        [14, 16],
+        [16, 18],
+        [18, 20],
+    ],
+    "faint": [
+        [19, 20],
+        [20, 21],
+    ],
+}
 
 # Compute correlation function
 print("Computing 2PCF")
@@ -68,18 +81,44 @@ nbins = 15
 bins = np.logspace(np.log10(0.01), np.log10(10.0), nbins + 1)
 nthreads = 1
 
-DD_counts = DDtheta_mocks(1, nthreads, bins, targets["RA"], targets["DEC"])
+# Pre-compute randoms-related data
 RR_counts = DDtheta_mocks(1, nthreads, bins, randoms["RA"], randoms["DEC"])
-DR_counts = DDtheta_mocks(0, nthreads, bins, targets["RA"], targets["DEC"], RA2=randoms["RA"], DEC2=randoms["DEC"])
+randoms_count = len(randoms["RA"])
+print("Randoms count:", randoms_count)
 
-wtheta = convert_3d_counts_to_cf(
-    targets_count, targets_count,
-    randoms_count, randoms_count,
-    DD_counts, DR_counts,
-    DR_counts, RR_counts
-)
+for bgs_category_name, ids in bgs_category_target_ids.items():
+    print(bgs_category_name)
+    bgs_category_filtered_targets = {
+        k: v[ids]
+        for k, v in targets.items()
+    }
 
-with open(f"/cluster/scratch/lmachado/DataProducts/2PCF/{REGION}_2PCF_bins.npy", "wb") as f:
-    np.save(f, bins[:-1])
-with open(f"/cluster/scratch/lmachado/DataProducts/2PCF/{REGION}_2PCF_wtheta.npy", "wb") as f:
-    np.save(f, wtheta)
+    for rmag_low, rmag_high in rmag_bins[bgs_category_name]:
+        rmag_ids = np.where(
+            (rmag_low <= bgs_category_filtered_targets["MAG_R"]) &
+            (bgs_category_filtered_targets["MAG_R"] < rmag_high)
+        )[0]
+        rmag_filtered_targets = {
+            k: v[rmag_ids]
+            for k, v in bgs_category_filtered_targets.items()
+        }
+
+        # Count number of targets and randoms
+        targets_count = len(rmag_filtered_targets["RA"])
+
+        print("Targets count:", targets_count)
+
+        DD_counts = DDtheta_mocks(1, nthreads, bins, rmag_filtered_targets["RA"], rmag_filtered_targets["DEC"])
+        DR_counts = DDtheta_mocks(0, nthreads, bins, rmag_filtered_targets["RA"], rmag_filtered_targets["DEC"], RA2=randoms["RA"], DEC2=randoms["DEC"])
+
+        wtheta = convert_3d_counts_to_cf(
+            targets_count, targets_count,
+            randoms_count, randoms_count,
+            DD_counts, DR_counts,
+            DR_counts, RR_counts
+        )
+
+        with open(f"/cluster/scratch/lmachado/DataProducts/2PCF/{REGION}_2PCF_bins_{bgs_category_name}_rmag_range{rmag_low:.1f}-{rmag_high:.1f}.npy", "wb") as f:
+            np.save(f, bins[:-1])
+        with open(f"/cluster/scratch/lmachado/DataProducts/2PCF/{REGION}_2PCF_wtheta_{bgs_category_name}_rmag_range{rmag_low:.1f}-{rmag_high:.1f}.npy", "wb") as f:
+            np.save(f, wtheta)
--- desiimaginganalysis/plot_2PCF.py
+++ desiimaginganalysis/plot_2PCF.py
@@ -1,16 +1,45 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import os
 
 REGION = "north"
 
-with open(f"/cluster/scratch/lmachado/DataProducts/2PCF/{REGION}_2PCF_bins.npy", "rb") as f:
-    bins = np.load(f)
-with open(f"/cluster/scratch/lmachado/DataProducts/2PCF/{REGION}_2PCF_wtheta.npy", "rb") as f:
-    wtheta = np.load(f)
+# Obtain list of 2PCF files available
+PATH_TO_2PCF_FILES = "/cluster/scratch/lmachado/DataProducts/2PCF/"
 
-plt.plot(bins, wtheta)
+FILENAMES = os.listdir(PATH_TO_2PCF_FILES)
 
-plt.xscale("log")
-plt.yscale("log")
+# Focus on desired region, on _bins_ files, and on bright vs. faint
+FILENAMES = [f for f in FILENAMES if f.startswith(REGION) and "_bins_" in f]
 
-plt.show()
+TYPE_FILENAMES = {
+    "bright": [f for f in FILENAMES if "bright" in f],
+    "faint": [f for f in FILENAMES if "faint" in f],
+}
+
+def get_rmag_range_from_filename(filename):
+    rmag_range = filename[
+        filename.find("range")+len("range"):
+        filename.find(".npy")
+    ]
+    rmag_low, rmag_high = rmag_range.split("-")
+    return float(rmag_low), float(rmag_high)
+
+for type_targets, filelist in TYPE_FILENAMES.items():
+    for filename in filelist:
+        with open(f"{PATH_TO_2PCF_FILES}/{filename}", "rb") as f:
+            bins = np.load(f)
+
+        rmag_low, rmag_high = get_rmag_range_from_filename(filename)
+
+        with open(f"{PATH_TO_2PCF_FILES}/north_2PCF_wtheta_{type_targets}_rmag_range{rmag_low:.1f}-{rmag_high:.1f}.npy", "rb") as f:
+            wtheta = np.load(f)
+
+        plt.plot(bins, wtheta, label=f"{rmag_low} - {rmag_high}")
+
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.title(type_targets)
+    plt.legend()
+
+    plt.show()
